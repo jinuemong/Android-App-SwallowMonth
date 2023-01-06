5653419e0968646894183fbe5177c3175f4af560
jinuemong <jinwoo3661@naver.com>
2023-01-06 19:11:49 +0900

    20230106

---
+++ app/src/main/java/com/example/SwallowMonthJM/Calendar/CalendarAdapterStatistics.kt
@@ -0,0 +1,18 @@
+package com.example.SwallowMonthJM.Calendar
+
+import android.widget.LinearLayout
+import androidx.fragment.app.FragmentManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.SwallowMonthJM.MainActivity
+import java.util.*
+
+class CalendarAdapterStatistics(
+    private val mainActivity: MainActivity,
+    private val calendarLayout : LinearLayout,
+    private val fm : FragmentManager,
+    date : Date,
+    currentMonth : Int
+) :RecyclerView.Adapter<>{
+
+    inner class
+}
--- app/src/main/java/com/example/SwallowMonthJM/Statistics/StatisticsTopFragment.kt
+++ app/src/main/java/com/example/SwallowMonthJM/Statistics/StatisticsTopFragment.kt
@@ -1,8 +1,16 @@
 package com.example.SwallowMonthJM.Statistics
 
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.example.SwallowMonthJM.MainActivity
 import com.example.SwallowMonthJM.databinding.StatisticsTopViewBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class StatisticsTopFragment() : Fragment() {
     var pageIndex = 0
@@ -10,4 +18,52 @@ class StatisticsTopFragment() : Fragment() {
     private var _binding : StatisticsTopViewBinding?=null
     private val binding  get() = _binding!!
     lateinit var mainActivity: MainActivity
+    lateinit var currentDate:  Date
+    lateinit var fm : FragmentManager
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = StatisticsTopViewBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun initView(){
+        pageIndex -=(Int.MAX_VALUE / 2)
+        val date = Calendar.getInstance().run {
+            add(Calendar.MONTH,pageIndex)
+            time
+        }
+        currentDate = date
+
+        val dateTime:String = SimpleDateFormat(
+            "yyyy년 MM월", Locale.KOREA
+        ).format(date.time)
+
+        binding.stDate.text = dateTime
+        binding.calendarBox.fragCalenderRecycler.apply {
+
+        }
+    }
+
 }
