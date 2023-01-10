ffdf3d0da50d9e858b0c95db2184b1f2df3688fb
jinuemong <jinwoo3661@naver.com>
2023-01-10 12:24:10 +0900

    2023 01 10

--- app/src/main/java/com/example/SwallowMonthJM/IntroActivity.kt
+++ app/src/main/java/com/example/SwallowMonthJM/IntroActivity.kt
@@ -8,28 +8,33 @@ import com.example.SwallowMonthJM.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
     private lateinit var binding: ActivityIntroBinding
-    private var startIntroThread : StartIntroThread? = null
+    private var startIntroThread2 : StartIntroThread2? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityIntroBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.topText.animation = AnimationUtils.loadAnimation(this@IntroActivity, R.anim.enter_left)
-        binding.midText.animation = AnimationUtils.loadAnimation(this@IntroActivity, R.anim.enter_right)
-        binding.mainIntro.animation = AnimationUtils.loadAnimation(this@IntroActivity, R.anim.wave)
-        binding.bottomIntro.animation = AnimationUtils.loadAnimation(this@IntroActivity, R.anim.enter_up)
-
-        startIntroThread = StartIntroThread()
-        startIntroThread?.start()
-
-        val intent = Intent(applicationContext,MainActivity::class.java)
-        startActivity(intent)
     }
-    inner class StartIntroThread : Thread(){
+
+    override fun onStart() {
+        super.onStart()
+        initAni()
+        startIntroThread2 = StartIntroThread2()
+        startIntroThread2?.start()
+    }
+    private fun initAni(){
+        binding.topText.startAnimation(AnimationUtils.loadAnimation(this@IntroActivity, R.anim.enter_left))
+        binding.midText.startAnimation(AnimationUtils.loadAnimation(this@IntroActivity, R.anim.enter_right))
+        binding.mainIntro.startAnimation(AnimationUtils.loadAnimation(this@IntroActivity, R.anim.wave))
+        binding.bottomIntro.startAnimation(AnimationUtils.loadAnimation(this@IntroActivity, R.anim.enter_up))
+    }
+    inner class StartIntroThread2: Thread(){
         override fun run() {
             super.run()
             try {
-                sleep(5000)
+                sleep(3000)
+                val intent = Intent(applicationContext,MainActivity::class.java)
+                startActivity(intent)
             } catch (e: InterruptedException) {
                 e.printStackTrace()
             }
