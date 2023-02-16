4257a3b87cc5371fdc2ef44a12ec38cbbad78e04
김진우 <jinwoo3661@naver.com>
2023-02-17 01:39:46 +0900

    20230217

--- app/src/main/java/com/example/SwallowMonthJM/UIFragment/ProfileUpdateFragment.kt
+++ app/src/main/java/com/example/SwallowMonthJM/UIFragment/ProfileUpdateFragment.kt
@@ -6,42 +6,91 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.example.SwallowMonthJM.MainActivity
+import com.example.SwallowMonthJM.Network.MasterApplication
 import com.example.SwallowMonthJM.R
 import com.example.SwallowMonthJM.databinding.FragmentProfileUpdateBinding
+import com.example.SwallowMonthJM.Manager.UserManager
+import com.example.SwallowMonthJM.Model.Profile
 
-class ProfileUpdateFragment : Fragment(){
-    private var _binding : FragmentProfileUpdateBinding? = null
+class ProfileUpdateFragment : Fragment() {
+    private var _binding: FragmentProfileUpdateBinding? = null
     private val binding get() = _binding!!
     private lateinit var mainActivity: MainActivity
-
+    private lateinit var userManager: UserManager
+    private var updateProfile : Profile? = null
+    private var imageUri = ""
+    private val selectPicFragment=  SelectPicFragment()
     override fun onAttach(context: Context) {
         super.onAttach(context)
         mainActivity = context as MainActivity
-
+        userManager = UserManager(mainActivity.application as MasterApplication, mainActivity)
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentProfileUpdateBinding.inflate(inflater,container,false)
+        _binding = FragmentProfileUpdateBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        mainActivity.viewModel.profile.apply {
+            updateProfile = this
+            Glide.with(mainActivity)
+                .load(userImage)
+                .into(binding.userImage)
 
+            binding.insertId.setText(userName)
+            binding.insertComment.setText(userComment)
+        }
         setUpListener()
+        selectPicFragment.apply {
+            setOnItemClickListener(object :SelectPicFragment.OnItemClickListener{
+                override fun onItemClick(lastUri: String) {
+                    if (imageUri!=""){
+                        Glide.with(mainActivity)
+                            .load(lastUri)
+                            .into(binding.userImage)
+                        imageUri = lastUri
+                    }else{
+                        //사진이 없다면 원래 이미지 등록
+                        Glide.with(mainActivity)
+                            .load(mainActivity.viewModel.profile.userImage)
+                            .into(binding.userImage)
+                    }
+                }
+            })
+        }
     }
 
-    private fun setUpListener(){
+    private fun setUpListener() {
         binding.backButton.setOnClickListener {
             mainActivity.onFragmentGoBack(this@ProfileUpdateFragment)
         }
 
+        //이미지 변경
+        binding.userImage.setOnClickListener {
+
+            mainActivity.onFragmentChange(SelectPicFragment())
+        }
         //제출
         binding.commitButton.setOnClickListener {
-
+            updateProfile?.let { up ->
+                userManager.setUserProfile(up,imageUri, paramFun = { newProfile,erMessage->
+                    if (newProfile != null && erMessage=="") {
+                        mainActivity.viewModel.profile = newProfile
+                    }else{
+                        //에러 메시지 띄우기
+                        Toast.makeText(mainActivity.applicationContext,erMessage
+                            ,Toast.LENGTH_SHORT).show()
+                    }
+                })
+            }
         }
     }
 }
--- app/src/main/java/com/example/SwallowMonthJM/UIFragment/SelectPicFragment.kt
+++ app/src/main/java/com/example/SwallowMonthJM/UIFragment/SelectPicFragment.kt
@@ -1,17 +1,51 @@
 package com.example.SwallowMonthJM.UIFragment
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity.RESULT_OK
+import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.MediaStore.Video.Media
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.bumptech.glide.Glide
+import com.example.SwallowMonthJM.Adapter.SelectPicAdapter
+import com.example.SwallowMonthJM.MainActivity
 import com.example.SwallowMonthJM.R
 import com.example.SwallowMonthJM.databinding.FragmentSelectPicBinding
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
+import org.mozilla.javascript.tools.jsc.Main
+import java.text.SimpleDateFormat
+
 
 class SelectPicFragment : Fragment() {
     private var _binding : FragmentSelectPicBinding? = null
     private val binding  get() = _binding!!
+    private lateinit var permissionListener : PermissionListener
+    private lateinit var mainActivity:MainActivity
+    private var lastUri = ""
 
+    private var onItemClickListener : OnItemClickListener?= null
+
+    interface OnItemClickListener{
+        fun onItemClick(lastUri:String){}
+    }
+    fun setOnItemClickListener(listener: OnItemClickListener){
+        this.onItemClickListener = listener
+    }
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -22,8 +56,129 @@ class SelectPicFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        view.animation = mainActivity.aniList[1]
+        view.animation.start()
+        //권한 확인
+        permissionListener = object : PermissionListener{
+            override fun onPermissionGranted() {
+                val adapter = SelectPicAdapter(mainActivity,getGallery())
+                binding.imageRecycler.adapter = adapter
+                    .apply {
+                        setOnItemClickListener(object : SelectPicAdapter.OnItemClickListener{
+                            override fun onItemClick(imageUri: String) {
+                                if (imageUri==""){
+                                    binding.selectImage.setImageResource(0)
+                                    lastUri = imageUri
+                                }else if (imageUri=="camera!"){
+                                    //카메라 실행 코드
+                                    openCamera()
+                                }else{
+                                    Glide.with(mainActivity)
+                                        .load(imageUri)
+                                        .into(binding.selectImage)
+                                    lastUri = imageUri
+                                }
+                            }
+                        })
+                    }
+            }
 
+            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                Toast.makeText(mainActivity,"권한 필요", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+        }
+
+
+        //권한 적용
+        TedPermission.with(mainActivity)
+            .setPermissionListener(permissionListener)
+            .setPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ).check()
+
+        setUpListener()
     }
 
+    private fun setUpListener(){
+        binding.cancelButton.setOnClickListener {
+            mainActivity.onFragmentGoBack(this@SelectPicFragment)
+        }
+
+        binding.uploadButton.setOnClickListener {
+            mainActivity.onFragmentGoBack(this@SelectPicFragment)
+            onItemClickListener?.onItemClick(lastUri)
+        }
+    }
+
+    @SuppressLint("Recycle")
+    private fun getGallery() :ArrayList<String>{
+        val picList = ArrayList<String>()
+        picList.add("") //첫번째는 카메라
+        val uriExternal : Uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        var columnIndexId : Int
+        val cursor = mainActivity.contentResolver.query(
+            uriExternal,null,null,null,
+            MediaStore.Images.ImageColumns.DATE_TAKEN+" DESC"
+        )
+        if (cursor!=null){
+            while (cursor.moveToNext()){
+                columnIndexId = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+                val imageId = cursor.getLong(columnIndexId)
+                val uriImage = Uri.withAppendedPath(uriExternal,""+imageId)
+                picList.add(uriImage.toString())
+            }
+            cursor.close()
+        }
+        return picList
+    }
+
+    //카메라 촬영
+    private fun openCamera(){
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        createImageUri(newFileName())?.let {uri ->
+            intent.putExtra(MediaStore.EXTRA_OUTPUT,uri)
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+                if (it.resultCode ==RESULT_OK){
+                    lastUri = getImageFilePath(uri)
+                    Glide.with(mainActivity)
+                        .load(lastUri)
+                        .into(binding.selectImage)
+                }
+            }
+        }
+    }
+
+    //새 파일 이름 생성
+    @SuppressLint("SimpleDateFormat")
+    private fun newFileName() : String {
+        val sdf = SimpleDateFormat("yyyyMMdd_HHmmSS")
+        return "${sdf.format(System.currentTimeMillis())}.jpg"
+    }
+
+    //이미지 파일 생성 함수
+    private fun createImageUri(filename : String) : Uri?{
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.DISPLAY_NAME,filename)
+        values.put(MediaStore.Images.Media.MIME_TYPE,"image/jpg")
+
+        return mainActivity.contentResolver
+            .insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values)
+    }
+
+    @SuppressLint("Recycle")
+    private fun getImageFilePath(uri: Uri) : String{
+        var columIndex = 0
+        val projection = arrayOf(MediaStore.Images.Media.DATA)
+        val cursor = mainActivity.contentResolver.query(uri,projection,null,null,null)
+        if(cursor!!.moveToFirst()){
+            columIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        }
+
+        return cursor.getString(columIndex)
+    }
 
 }
